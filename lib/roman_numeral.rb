399b9451dcae31dbe6343e5eab99f338afdc25ef
Lewis Reid <lreidwilliams@gmail.com>
2020-08-19 00:25:04 +1000

    1 step forward 2 steps backwards with substitution rule

--- lib/roman_numeral.rb
+++ lib/roman_numeral.rb
@@ -1,3 +1,5 @@
+# TODO: Replace nil argument with Null Object
+# TODO: Find a better way to handle the substitution case
 class RomanNumeral
     def initialize(character, quantity: 1, next_glyph: nil)
         @character = character
@@ -6,13 +8,28 @@ class RomanNumeral
     end
 
     def convert(n)
-        amount = (n / @quantity).floor
-        @character * amount + convert_using_next(n - amount * @quantity).to_s
+        if can_handle?(n)
+            @character * occurences_in(n) + convert_using_next(remainder(n))
+        else
+           @next_glyph&.convert(1).to_s + convert(n.succ)
+        end
+    end
+
+    def can_handle?(n)
+        occurences_in(n) < 4 && (@next_glyph.nil? || @next_glyph.can_handle?(remainder(n)))
     end
 
     private
 
+    def remainder(n)
+        n - occurences_in(n) * @quantity
+    end
+
+    def occurences_in(n)
+        (n / @quantity).floor
+    end
+
     def convert_using_next(n)
-        n.positive? ? @next_glyph&.convert(n) : nil
+        @next_glyph&.convert(n).to_s
     end
 end
